35104c6bef20bc6b398e406e5e5b2800a0766d41
attacktrip <semenklementev50@gmail.com>
2026-02-10 22:10:47 +0300

    Update app.py

--- site/backend/app.py
+++ site/backend/app.py
@@ -815,4 +815,5 @@ if __name__ == '__main__':
     threading.Thread(target=run_bot, daemon=True).start()
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, use_reloader=False)
+    app.run(host='0.0.0.0', port=port)
+
